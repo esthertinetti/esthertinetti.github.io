d97ee38499584d8a176862f301a4843ce5019cf6
Esther Tinetti <esthertinetti@gmail.com>
2020-10-26 09:21:26 -0300

    ajustes

---
+++ precache-manifest.3e684b32f7436bd4046bbedbc8de8932.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ac674a9497f9a2f653185c6cbcdbca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0a0367c7b5a08ea34c",
+    "url": "/static/css/main.1d51b7ac.chunk.css"
+  },
+  {
+    "revision": "afc1f5ed959bd40f5233",
+    "url": "/static/js/2.56a793dd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.56a793dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0a0367c7b5a08ea34c",
+    "url": "/static/js/main.ed2155d6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "9eec66742fa9b15e13d9d3f7f15465c2",
+    "url": "/static/media/logo.9eec6674.svg"
+  }
+]);
